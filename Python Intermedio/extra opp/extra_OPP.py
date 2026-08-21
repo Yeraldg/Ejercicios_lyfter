9a3dfaaa5c284ece1467345433d32166fd8fecf8
Yerald Mauricio Garcia Arguedas <yeralg220187@gmail.com>
2026-08-21 16:58:00 -0600

    Complete OOP extra exercises 1, 2 and 3

--- Python Intermedio/extra opp/extra_OPP.py
+++ Python Intermedio/extra opp/extra_OPP.py
@@ -15,7 +15,7 @@ class Rectangle:
         
         return perimeter
     
-def  get_rectangle_values():
+def get_rectangle_values():
 
     while True:
         try:
@@ -24,7 +24,7 @@ def  get_rectangle_values():
             valid_values = True
             for value in (width, height):
                 if value <= 0:
-                    print("please entre a digit greater than 0")
+                    print("please enter a digit greater than 0")
                     valid_values = False
                     break
             if not valid_values:
@@ -32,7 +32,7 @@ def  get_rectangle_values():
             return width, height
         
         except ValueError:
-            print("please use only a number format.")
+            print("Please enter a valid format number.")
             continue
         
         
@@ -52,15 +52,48 @@ class Dog(Animal):
 class Cat(Animal):
     def speak(self):
         return "Miau"
+    
+    
+#ejercicio 3
+
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+class Inventory:
+    def __init__(self):
+        self.products = []
+        
+    def add_product(self, product):
+        self.products.append(product)
+        
+    def show_products(self):
+        print("Products on the inventory:")
+        for product in self.products:
+            print(f"Name: {product.name}")
+            print(f"Price: {product.price}")
+            print(f"Quantity: {product.quantity} units.")
+            print("--------------")
+            
+    def calculate_total_inventory_value(self):
+        total_value = 0
+        for product in self.products:
+            product_value = product.price * product.quantity
+            total_value += product_value
+        return total_value
+
+        
 
 
 def main():
     width, height = get_rectangle_values()
-    rectangle = Rectangle(width,height)
+    rectangle = Rectangle(width, height)
     area = rectangle.get_area()
     perimeter = rectangle.get_perimeter()
-    print(f" the area is: {area}")
-    print(f" the perimeter is: {perimeter}")
+    print(f"the area is: {area}")
+    print(f"the perimeter is: {perimeter}")
     
     dog = Dog("firulais")
     print(f"the dog's name is: {dog.name}")
@@ -68,6 +101,19 @@ def main():
     cat = Cat("Papi")
     print(f"the cat's name is: {cat.name}")
     print(cat.speak())
+    
+    product1 = Product("Mouse", 5000, 3)
+    product2 = Product("keyboard", 8000, 2)
+    inventory = Inventory()
+    inventory.add_product(product1)
+    inventory.add_product(product2)
+    print("Mouse and keyboard added")
+    
+
+    inventory.show_products()
+    
+    total = inventory.calculate_total_inventory_value()
+    print(f"Total value of entire inventory: {total}")
     
 main()
 
