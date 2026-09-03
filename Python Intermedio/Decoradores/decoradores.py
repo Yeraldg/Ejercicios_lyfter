4cbbe315bbfe07fab937518de4842fe4f3ed0bec
Yerald Mauricio Garcia Arguedas <yeralg220187@gmail.com>
2026-09-03 16:05:43 -0600

    Fix age property tuple comparison

--- Python Intermedio/Decoradores/decoradores.py
+++ Python Intermedio/Decoradores/decoradores.py
@@ -44,7 +44,7 @@ class User:
     def age(self):
         today = date.today()
         age = today.year - self.date_of_birth.year
-        if(today.month, today.day) < (self.date_of_birth.month, self,self.date_of_birth.day):
+        if(today.month, today.day) < (self.date_of_birth.month,self.date_of_birth.day):
             age -= 1
         return age
             
